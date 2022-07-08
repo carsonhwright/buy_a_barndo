e8c9f2a967cf48272d02b12fe47ad5ee420920f6
carsonhwright <carsonhwright@gmail.com>
2022-07-07 22:23:06 -0400

    constructs lists of dicts of entries for house listings, next steps are to expand and automate database updates and implement analysis and scoring of house listings

--- zillow_html_parser.py
+++ zillow_html_parser.py
@@ -7,7 +7,7 @@ class MyHTMLParser(HTMLParser):
         
         '''print("Encountered a start tag:", tag)
         if tag == 'script' and attrs == [('data-zrr-key', 'static-search-page:search-app')]:
-            breakpoint()'''
+        '''
 
     def handle_endtag(self, tag):
         '''print("Encountered an end tag :", tag)'''
@@ -16,22 +16,8 @@ class MyHTMLParser(HTMLParser):
         '''print("Encountered some data  :", data)'''
         if "<!--{\"queryState\":{\"pagination\":{},\"usersSearchTerm\":" in data:
             self.desired_data = data
-            # print(data)
-
-    def handle_scraper_output(self, data):
-        """
-        
-        """
-        parser = MyHTMLParser()
-        with open('output/Fulton County.html', 'r', encoding='utf-8') as f:
-            html_feed = f.read()
-
-        parser.feed(html_feed)
-        desired_html_regex = re.compile("(?!<!--)\{[\s\S\n]*(?<=)}")
-        clean_dict = re.search(desired_html_regex, parser.desired_data).group(0)
-        clean_actual_dict = json.loads(clean_dict)
-
-        # individual houses are clean_actual_dict["cat1"]["searchResults"]["listResults"][list_index]
-        # at this point I can parse desired html data from each house, now I need to implement
-        for unit_index in range(len(clean_actual_dict["cat1"]["searchResults"]["listResults"])):
-            print(clean_actual_dict["cat1"]["searchResults"]["listResults"][unit_index]["price"])
+            desired_html_regex = re.compile("(?!<!--)\{[\s\S\n]*(?<=)}")
+            clean_dict = re.search(desired_html_regex, self.desired_data).group(0)
+            clean_actual_dict = json.loads(clean_dict)
+            with open('output\\temp.json', 'w', encoding='utf-8') as g:
+                json.dump(clean_actual_dict, g, ensure_ascii=False, indent=4)
